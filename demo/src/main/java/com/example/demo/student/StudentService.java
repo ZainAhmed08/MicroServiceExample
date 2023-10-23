f58e2f4a51518dc29ee81537a7814f3c6ab6d1e7
zain ahmed <zainahmed01997@gmail.com>
2023-10-24 03:17:24 +0530

    it only has get and post methods implimented the rest is easy to impliment and since it's only a refrence it can we left incomplete

---
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,34 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service //@Service/@Component are exactly the same and basically help with @AutoWiring to achieve Dependency injection.
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents(){
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()){
+            throw new IllegalStateException("Email already taken");
+        }
+        studentRepository.save(student);
+    }
+}
